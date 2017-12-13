40c701668157614c113568f9a6c5a277aa079cb9
MapleinCode <llkj13579@126.com>
2017-12-13 10:51:07 +0800

    feat: first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Config = require('./lib/configw');
+
+const rootPath = process.env['CONFIGW_ROOT_PATH'] || process.cwd();
+
+const config = new Config(rootPath);
+
+module.export = config;
